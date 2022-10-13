5d1cb9d87807adabc678a4d53242b58f684ddb90
Владимир Тимаков <timaone@yandex.ru>
2022-10-13 22:21:17 +0300

    wf30

--- backend/foodgram/api/permissions.py
+++ backend/foodgram/api/permissions.py
@@ -3,8 +3,8 @@ from rest_framework import permissions
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        return bool(request.method in permissions.SAFE_METHODS or
-                    request.user and request.user.is_authenticated)
+        return (request.method in permissions.SAFE_METHODS or
+                request.user and request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
         return (
